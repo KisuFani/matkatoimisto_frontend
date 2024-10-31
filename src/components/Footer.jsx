bc8245a30d871bd3f29d67f106ca06b45539f26b
KatjaK <akk45885@edu.redu.fi>
2024-10-31 10:48:13 +0200

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function Footer(){
+    return(
+      <>
+      <div className='background-color-footer'>
+        <p>
+            footer
+        </p>
+
+
+      </div>
+      </>  
+    )
+}
+
+export default Footer
